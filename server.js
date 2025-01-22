5c61a0aeb73cb78aa035f720139b5f75565b55bd
Dhriti <dhriti.baruah20@gmail.com>
2025-01-22 22:41:20 +0530

    Successfully connected with Mongodb's cloud storage

--- server.js
+++ server.js
@@ -4,19 +4,27 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const PORT = 4000;
 const Note = require("./models/Note");
+require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //Connect to mongodb
-mongoose
-  .connect("mongodb://0.0.0.0:27017/reactNote", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("DB connected"))
-  .catch((err) => console.log(err));
+// mongoose
+//   .connect("mongodb://0.0.0.0:27017/reactNote", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("DB connected"))
+//   .catch((err) => console.log(err));
+
+//Cloud Database
+const db = process.env.MONGO_URI;
+
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('DB connected'))
+    .catch(err => console.log(err));
 
 // CREATE
 app.post("/addNote", (req, res) => {
